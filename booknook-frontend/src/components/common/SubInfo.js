7038e77dea6895ebbb4ee35f4980a57a95becdb5
syk2021 <85333719+syk2021@users.noreply.github.com>
2022-07-28 17:21:07 +0900

    Update SubInfo.js

--- booknook-frontend/src/components/common/SubInfo.js
+++ booknook-frontend/src/components/common/SubInfo.js
@@ -15,12 +15,11 @@ const SubInfoBlock = styled.div`
       margin-top: 1rem;
     `}
   color: ${palette.gray[6]};
-  /* span 사이에 가운뎃점 문자 보여주기*/
   span + span:before {
     color: ${palette.gray[4]};
     padding-left: 0.25rem;
     padding-right: 0.25rem;
-    content: '\\B7'; /* 가운뎃점 문자 */
+    content: '\\B7';
   }
 `;
 
